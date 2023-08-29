d67f1a9a7ce37900ff764ddbf6067d27d544f2df
zhangweiwei <zhangweiwei@myai.tech>
2023-08-29 23:43:12 +0800

    KMP算法的学习_核心思想和算法理解

--- src/main/java/com/wfz/arithmetic/kmp/StringMatch_01.java
+++ src/main/java/com/wfz/arithmetic/kmp/StringMatch_01.java
@@ -1,4 +1,18 @@
 package com.wfz.arithmetic.kmp;
 
 public class StringMatch_01 {
+
+    public static void main(String[] args) {
+
+        String str1 = "BBC ABCDAB ABCDABCDABDE";
+        String str2 = "ABCDABD";
+
+    }
+
+    //获取到一个字符串（子串）的部分匹配值表
+    public static int[] kmpNex(String dest) {
+        int[] nexts = new int[dest.length()];
+
+        return nexts;
+    }
 }
